093d3f0eb688ba9ff2d892611370a42f333b3983
eric <e.adelaide.beaubrun@gmail.com>
2026-07-06 02:41:12 +0200

    Fix styles for contact overlay, profil picture and navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar = ({lenis, activeSection}: NavbarProps) => {
                     to={target}
                     smooth={true}
                     duration={DURATION}
-                    offset={OFFSET}
+                    offset={target === "projects" ? OFFSET + 500 : OFFSET}
                     onClick={handleClick}
                 >
                     <div className={`item ${isActive ? 'active' : ''}`} data-target={target}>
@@ -104,9 +104,9 @@ const Navbar = ({lenis, activeSection}: NavbarProps) => {
                     </>
                 ) : (
                     <>
-                        <div className="setting setting-left">
-                            <LanguageSwitcher/>
-                        </div>
+                        {/*<div className="setting setting-left">*/}
+                        {/*    <LanguageSwitcher/>*/}
+                        {/*</div>*/}
                         {renderNavItems()}
                         <div className="setting setting-right">
                             {lenis && <ScrollProgressCircle lenis={lenis}/>}
